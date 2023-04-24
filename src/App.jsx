894a1b6a88cb32fca773e1cf76d6d0aa60dd90f2
mokava <romeramatias@gmail.com>
2023-04-24 20:42:02 -0300

    Create dashboard skeleton

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
-function App() {
+import { Route, Routes } from "react-router";
+import Ordenes from "./components/paginas/Ordenes";
+import Menu from "./components/paginas/Menu";
+import NuevoPlatillo from "./components/paginas/NuevoPlatillo";
+import Sidebar from "./components/ui/Sidebar";
+
+const App = () => {
   return (
-    <>
-      <h1 className="text-3xl font-bold text-red-500 underline text-center">
-        Hello world!
-      </h1>
-    </>
+    <div className="md:flex min-h-screen">
+      <Sidebar />
+      <div className="md:w-3/5 xl:w-4/5">
+        <Routes>
+          <Route path="/" element={<Ordenes />} />
+          <Route path="/menu" element={<Menu />} />
+          <Route path="/nuevo-platillo" element={<NuevoPlatillo />} />
+        </Routes>
+      </div>
+    </div>
   );
-}
+};
 
 export default App;
